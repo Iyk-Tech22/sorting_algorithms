cfb236e8ec7411293aa60510634683fdaace016b
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-11-08 15:08:28 +0100

    Fixing issues

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,59 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-// protype functions
-void swap(int *a, int *b);
-
 /**
- * bubble_sort - it a comparison algorithm, that compare pairs of adjecent
- * @array: Is the Data Structure to sort\
- * @size: Size of the Data Structure
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-    // create a variable
-    int i, j, is_swap;
+	size_t i, n, new_n;
+	int swap;
 
-    // if size is <= 2 break
-        if (size < 2)
-            return;
-    // loop from i to the size of the array
-    for (i = 1; i < size; ++i)
-    {
-        
-        is_swap = 0;
-        // loop from j to size-1
-        for (j = 0; j < size-1-i; ++j)
-        {
-            // if elem at j < j+1
-            if (array[j] > array[j+1])
-            {
-                // swap
-                swap(array[j], array[j+1]);
-                // set is_swap to 1
-                is_swap = 1;
-            }
-        }
-        // if is_swap is not 1 break from the loop
-        if (is_swap == 0)
-            return;
-        // print array after every swap
-        print_array(array, size);
-        printf("\n");
-    }
-}
+	if (array == NULL || size < 2)
+		return;
 
-/**
- * swap - Swap two value in array
- * @array: Array data structure
- * @idx_1: First value index
- * @idx_2: Second value index
- */
-void swap(int *a, int *b)
-{
-    int temp;
-    temp = *b;
-    *a = *b;
-    b = temp;
+	n = size;
+	while (n > 0)
+	{
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
+				print_array(array, size);
+			}
+		}
+		n = new_n;
+	}
 }
